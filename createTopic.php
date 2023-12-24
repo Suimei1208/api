b9ccc8a668a3d0d1ad47f729b6ef1f2cfb182446
Suimei1208 <103234140+Suimei1208@users.noreply.github.com>
2023-12-24 21:56:13 +0700

    ..

--- createTopic.php
+++ createTopic.php
@@ -13,8 +13,8 @@
 
         if ($topicName !== null && $description !== null && $isPublic !== null && $ownerID !== null) {
             if ($dbCon) {
-                $checkQuery = "SELECT * FROM [dbo].[Topic] WHERE topicName = ?";
-                $checkParams = array($topicName);
+                $checkQuery = "SELECT * FROM [dbo].[Topic] WHERE topicName = ? AND ownerID = ?";
+                $checkParams = array($topicName, $ownerID);
                 $checkStmt = sqlsrv_prepare($dbCon, $checkQuery, $checkParams);
 
                 if ($checkStmt && sqlsrv_execute($checkStmt)) {
@@ -31,13 +31,28 @@
                         $insertStmt = sqlsrv_prepare($dbCon, $insertQuery, $insertParams);
 
                         if ($insertStmt && sqlsrv_execute($insertStmt)) {
-                            $response['status'] = 'OK';
-                            $response['data'] = null;
-                            $response['message'] = 'Topic created successfully';
+                            // Lấy dữ liệu của chủ đề vừa được tạo
+                            $selectQuery = "SELECT * FROM [dbo].[Topic] WHERE topicName = ?";
+                            $selectParams = array($topicName);
+                            $selectStmt = sqlsrv_prepare($dbCon, $selectQuery, $selectParams);
+
+                            if ($selectStmt && sqlsrv_execute($selectStmt)) {
+                                $createdTopic = sqlsrv_fetch_array($selectStmt, SQLSRV_FETCH_ASSOC);
+
+                                $response['status'] = 'OK';
+                                $response['data'] = $createdTopic;
+                                $response['message'] = 'Topic created successfully';
+                            } else {
+                                $response['status'] = 'NOT OK';
+                                $response['message'] = 'Error fetching created topic: ' . print_r(sqlsrv_errors(), true);
+                            }
+
+                            sqlsrv_free_stmt($selectStmt);
                         } else {
                             $response['status'] = 'NOT OK';
                             $response['message'] = 'Error executing topic creation query';
                         }
+
                         sqlsrv_free_stmt($insertStmt);                      
                     }
                     sqlsrv_free_stmt($checkStmt);
