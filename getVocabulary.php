759daf3503b524bc328932f145f759a1f2aaf800
Suimei1208 <103234140+Suimei1208@users.noreply.github.com>
2023-12-24 10:40:26 +0700

    ...

---
+++ getVocabulary.php
@@ -0,0 +1,47 @@
+<?php
+header('Content-Type: application/json');
+
+require_once('connection.php');
+
+$response = array();
+
+if ($dbCon) {
+    $query = "SELECT * FROM [dbo].[Vocabulary]";
+    $stmt = sqlsrv_prepare($dbCon, $query);
+
+    if ($stmt && sqlsrv_execute($stmt)) {
+        $vocabularies = array();
+
+        while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+            $vocabulary = array(
+                'vocabID' => $row['vocabID'],
+                'vocabulary' => $row['vocabulary'],
+                'meaning' => $row['meaning'],
+                'topicID' => $row['topicID']
+            );
+
+            $vocabularies[] = $vocabulary;
+        }
+
+        $response['status'] = 'OK';
+        $response['data'] = $vocabularies;
+        $response['message'] = 'Vocabularies retrieved successfully';
+    } else {
+        $response['status'] = 'NOT OK';
+        $response['message'] = 'Error executing query: ' . print_r(sqlsrv_errors(), true);
+    }
+
+    if ($dbCon) {
+        sqlsrv_close($dbCon);
+    }
+} else {
+    $response['status'] = 'NOT OK';
+    $response['message'] = 'Error connecting to SQL Server';
+}
+
+if (!isset($response['data'])) {
+    $response['data'] = null;
+}
+
+echo json_encode($response);
+?>
---
+++ get_Topic.php
@@ -0,0 +1,48 @@
+<?php
+header('Content-Type: application/json');
+
+require_once('connection.php');
+
+$response = array();
+
+if ($dbCon) {
+    $query = "SELECT * FROM [dbo].[Topic]";
+    $stmt = sqlsrv_prepare($dbCon, $query);
+
+    if ($stmt && sqlsrv_execute($stmt)) {
+        $topics = array();
+
+        while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+            $topic = array(
+                'id' => $row['id'],
+                'topicName' => $row['topicName'],
+                'description' => $row['description'],
+                'isPublic' => $row['isPublic'],
+                'ownerID' => $row['ownerID']
+            );
+
+            $topics[] = $topic;
+        }
+
+        $response['status'] = 'OK';
+        $response['data'] = $topics;
+        $response['message'] = 'Topics retrieved successfully';
+    } else {
+        $response['status'] = 'NOT OK';
+        $response['message'] = 'Error executing query: ' . print_r(sqlsrv_errors(), true);
+    }
+
+    if ($dbCon) {
+        sqlsrv_close($dbCon);
+    }
+} else {
+    $response['status'] = 'NOT OK';
+    $response['message'] = 'Error connecting to SQL Server';
+}
+
+if (!isset($response['data'])) {
+    $response['data'] = null;
+}
+
+echo json_encode($response);
+?>
