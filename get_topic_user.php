e93c2777baaab8c74f00bf4c2593605234975274
Suimei1208 <103234140+Suimei1208@users.noreply.github.com>
2023-12-25 01:21:18 +0700

    ..

--- get_topic_user.php
+++ get_topic_user.php
@@ -19,15 +19,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $result = array();
 
                 while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+                    $folderID = $row['TopicID'];
+                    $infoQuery = "SELECT * FROM [dbo].[Topic] WHERE id = ?";
+                    $infoParams = array($folderID);
+                    $infoStmt = sqlsrv_prepare($dbCon, $infoQuery, $infoParams);
+
+                    if ($infoStmt && sqlsrv_execute($infoStmt)) {
+                        $infoResult = array();
+
+                        while ($infoRow = sqlsrv_fetch_array($infoStmt, SQLSRV_FETCH_ASSOC)) {
+                            $infoResult[] = $infoRow;
+                        }
+
+                        $row['additionalInfo'] = $infoResult;
+                    } else {
+                        $response['status'] = 'NOT OK';
+                        $response['message'] = 'Error executing additional info query';
+                        echo json_encode($response);
+                        exit(); // Exit if there is an error retrieving additional info
+                    }
+
+                    sqlsrv_free_stmt($infoStmt);
                     $result[] = $row;
                 }
 
                 $response['status'] = 'OK';
-                $response['message'] = 'Top records retrieved successfully';
+                $response['message'] = 'Records retrieved successfully';
                 $response['data'] = $result;
             } else {
                 $response['status'] = 'NOT OK';
-                $response['message'] = 'Error executing query to retrieve top records';
+                $response['message'] = 'Error executing query to retrieve records';
             }
 
             sqlsrv_free_stmt($stmt);
